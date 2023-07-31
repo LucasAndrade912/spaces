a6659741f6e0e63682a92a8fcdc059fadaac7a82
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2023-07-31 19:47:28 -0300

    feat(server): implement oauth flow with google by server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,36 @@
 import express from 'express';
+import axios from 'axios';
+import 'dotenv/config';
 
+const FRONT_END_URL = 'http://localhost:5173';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-	res.json({ message: 'Hello World' });
+app.get('/oauth', async (req, res) => {
+	const rootUrl = 'https://oauth2.googleapis.com/token';
+	const { code, state } = req.query;
+
+	const options = {
+		code,
+		client_id: process.env.GOOGLE_OAUTH_CLIENT_ID,
+		client_secret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+		redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT,
+		grant_type: 'authorization_code'
+	};
+
+	const { data } = await axios.post(
+		rootUrl,
+		options,
+		{
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}
+	);
+
+	const { data: user } = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${data.access_token}`);
+
+	res.redirect(`${FRONT_END_URL}${state}`);
 });
 
 app.listen(PORT, () => {
