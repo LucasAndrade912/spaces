6dab61c3f66458a95b649cc8d5c29d06846ab482
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2023-08-02 00:51:55 -0300

    feat(web): create authentication context to provide logged in user data

---
+++ web/src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from 'react';
+
+import { api } from '../lib/api';
+
+interface UserProps {
+	email: string;
+	profilePicture: string;
+}
+
+interface AuthContextProps {
+	user: UserProps | null;
+	isAuthenticated: boolean;
+}
+
+interface AuthContextProviderProps {
+	children: React.ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+	const [user, setUser] = useState<UserProps | null>(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+	async function fetchUserInfo() {
+		try {
+			const { data } = await api.get('/me');
+
+			setUser(data.user);
+			setIsAuthenticated(true);
+		} catch (error) {
+			setUser(null);
+			setIsAuthenticated(false);
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchUserInfo();
+	}, []);
+
+	return <AuthContext.Provider value={{ user, isAuthenticated }}>{children}</AuthContext.Provider>;
+}
