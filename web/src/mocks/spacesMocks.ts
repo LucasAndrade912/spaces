230b56f5ef4f915586b0da0d79fdfc4ed3cbfef6
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2023-08-08 16:15:36 -0300

    feat(web): create Home page

---
+++ web/src/mocks/spacesMocks.ts
@@ -0,0 +1,26 @@
+export const spacesMocks = [
+	{
+		id: '1',
+		title: 'IFPB - TSI Docs',
+		size: 125829120,
+		lastUpdate: '31 de jul. de 2023'
+	},
+	{
+		id: '2',
+		title: 'Monitoria LS 2023.2 - Folhas de Frequência',
+		size: 58720256,
+		lastUpdate: '12 de ago. de 2023'
+	},
+	{
+		id: '3',
+		title: 'Vídeos de gatinhos',
+		size: 2469606195.2,
+		lastUpdate: '7 de fev. de 2023'
+	},
+	{
+		id: '4',
+		title: 'PDF’s de estudo para Javascript',
+		size: 458227712,
+		lastUpdate: '18 de mai. de 2023'
+	},
+];
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,13 +1,114 @@
+import { Link } from 'react-router-dom';
+import { MonitorPlay, Files, Folder } from 'phosphor-react';
+import { Images, Plus } from '@phosphor-icons/react';
+
+import spacesLogo from '../assets/spacesLogo.svg';
 import { useAuth } from '../hook/useAuth';
+import { spacesMocks } from '../mocks/spacesMocks';
+
+import { Space } from '../components/Space';
+import { Button } from '../components/Button';
+import { Select } from '../components/Select';
+import { FileTypeQuantity } from '../components/FileTypeQuantity';
+
+const fileTypeQuantitys = [
+	{
+		title: 'Imagens',
+		quantity: 14,
+		icon: <Images size={24} color="#FFF" />,
+		bgColor: 'bg-custom-red',
+	},
+	{
+		title: 'Vídeos',
+		quantity: 21,
+		icon: <MonitorPlay size={24} color="#FFF" />,
+		bgColor: 'bg-purple-900',
+	},
+	{
+		title: 'Documentos',
+		quantity: 55,
+		icon: <Files size={24} color="#FFF" />,
+		bgColor: 'bg-custom-orange',
+	},
+	{
+		title: 'Outros',
+		quantity: 10,
+		icon: <Folder size={24} color="#FFF" />,
+		bgColor: 'bg-custom-rose',
+	},
+];
 
 export function Home() {
 	const { user } = useAuth();
+	const profilePicture = user?.profilePicture;
 
 	return (
 		<>
-			<div className="flex justify-center items-center h-screen flex-col">
-				<span>Email: {user?.email}</span>
-				<img src={user?.profilePicture} alt="User Profile" />
+			<div className="p-10">
+				<header className="flex justify-between items-center w-full">
+					<Link to="/">
+						<img src={spacesLogo} alt="Spaces Logo" />
+					</Link>
+
+					<img
+						src={profilePicture}
+						alt="Foto de perfil do usuário"
+						className="w-10 h-10 rounded-full"
+					/>
+				</header>
+
+				<section className="flex justify-between w-full mt-16">
+					<section className="flex gap-20">
+						{fileTypeQuantitys.map((item, index) => (
+							<FileTypeQuantity.Root key={index}>
+								<FileTypeQuantity.Icon className={item.bgColor}>{item.icon}</FileTypeQuantity.Icon>
+
+								<FileTypeQuantity.Content>
+									<h3 className="text-sm font-medium">{item.title}</h3>
+									<span className="text-[10px] font-normal">{item.quantity} items</span>
+								</FileTypeQuantity.Content>
+							</FileTypeQuantity.Root>
+						))}
+					</section>
+
+					<section className="flex gap-6">
+						<Select />
+
+						<Button.Root className="h-8 gap-2 px-3">
+							<Button.Icon>
+								<Plus size={16} color="#FFF" weight="bold" />
+							</Button.Icon>
+
+							<Button.Title className="text-sm">Criar Space</Button.Title>
+						</Button.Root>
+					</section>
+				</section>
+
+				<section className="mt-20">
+					<h2 className="font-play text-xl font-bold">Seus spaces</h2>
+
+					<div className="w-full mt-6">
+						<div className="grid grid-cols-table">
+							<span className="text-xs">Nome</span>
+							<span className="text-xs">Tamanho</span>
+							<span className="text-xs">Última modificação</span>
+						</div>
+
+						<div className="w-full h-px bg-black-700 my-6" />
+
+						<div className="flex flex-col gap-8">
+							{spacesMocks.map((space) => (
+								<Space
+									key={space.id}
+									id={space.id}
+									title={space.title}
+									size={space.size}
+									lastUpdate={space.lastUpdate}
+								/>
+							))}
+						</div>
+					</div>
+				</section>
 			</div>
 		</>
 	);
